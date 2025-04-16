1fed6cb200b25cf0f57ce692d411a74fc98a9cb9
Hyovin Kwak <hyovin.kwak@tu-dortmund.de>
2025-04-16 23:44:21 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,5 +6,10 @@
 # set the shell prompt to a standard format.
 echo 'PS1="\u@\h:\w\$ "' >> ~/.bashrc
 
+#TODO: see if this code can work
+#if [[ $TERM == "dumb" ]]; then
+#    export PS1="$ "
+#fi
+
 # install clangd to enable lsp-mode
 sudo apt install clangd
