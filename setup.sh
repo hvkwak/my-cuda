8076008beca1da2551bcbdc2b01cef0172f751e5
Hyovin Kwak <hyovin.kwak@tu-dortmund.de>
2025-04-18 11:31:34 +0000

    setup.sh updates

--- setup.sh
+++ setup.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env sh
 
 # disable tmux
-# touch ...
+touch ~/.no_auto_tmux
 
 # set the shell prompt to a standard format.
-echo 'PS1="\u@\h:\w\$ "' >> ~/.bashrc
+echo '# Set the shell prompt to a standard format for tramp in emacs' >> ~/.bashrc
 
-#TODO: see if this code can work
-#if [[ $TERM == "dumb" ]]; then
-#    export PS1="$ "
-#fi
+# Option 1
+# echo 'PS1="\u@\h:\w\$ "' >> ~/.bashrc
+
+# Option 2
+cat << 'EOF' >> ~/.bashrc
+if [[ $TERM == "dumb" ]]; then
+  PS1='$ '
+fi
+EOF
 
 # install clangd to enable lsp-mode
 sudo apt install clangd
