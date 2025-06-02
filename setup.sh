867d0da43a5c597de51f39f282c8f607371daf17
Hyovin Kwak <hyovin.kwak@tu-dortmund.de>
2025-06-02 12:32:55 +0000

    setup.sh update

--- setup.sh
+++ setup.sh
@@ -31,5 +31,5 @@ git config --global user.email "hyovin.kwak@tu-dortmund.de"
 git config --global user.name "Hyovin Kwak"
 
 # install clangd to enable lsp-mode
-sudo apt install clangd
-#sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-17 100
+sudo apt install clangd-14
+sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-14 100
